c5511262f3f2e2daf6b258844b6778ad3d9405c7
Mingyang Zheng <mizhe@microsoft.com>
2022-07-01 16:54:43 -0700

    ListVmssFlexVMs

--- pkg/azureclients/vmclient/azure_vmclient.go
+++ pkg/azureclients/vmclient/azure_vmclient.go
@@ -217,6 +217,76 @@ func (c *Client) listVM(ctx context.Context, resourceGroupName string) ([]comput
 	return result, nil
 }
 
+// List gets a list of VirtualMachine in the resourceGroupName.
+func (c *Client) ListVmssFlexVMs(ctx context.Context, resourceGroupName string, vmssFlexID string) ([]compute.VirtualMachine, *retry.Error) {
+	mc := metrics.NewMetricContext("vm", "list", resourceGroupName, c.subscriptionID, "")
+
+	// Report errors if the client is rate limited.
+	if !c.rateLimiterReader.TryAccept() {
+		mc.RateLimitedCount()
+		return nil, retry.GetRateLimitError(false, "VMList")
+	}
+
+	// Report errors if the client is throttled.
+	if c.RetryAfterReader.After(time.Now()) {
+		mc.ThrottledCount()
+		rerr := retry.GetThrottlingError("VMList", "client throttled", c.RetryAfterReader)
+		return nil, rerr
+	}
+
+	result, rerr := c.listVmssFlexVMs(ctx, resourceGroupName, vmssFlexID)
+	mc.Observe(rerr)
+	if rerr != nil {
+		if rerr.IsThrottled() {
+			// Update RetryAfterReader so that no more requests would be sent until RetryAfter expires.
+			c.RetryAfterReader = rerr.RetryAfter
+		}
+
+		return result, rerr
+	}
+
+	return result, nil
+}
+
+// listVM gets a list of VirtualMachines in the resourceGroupName.
+func (c *Client) listVmssFlexVMs(ctx context.Context, resourceGroupName string, vmssFlexID string) ([]compute.VirtualMachine, *retry.Error) {
+	resourceID := armclient.GetResourceListID(c.subscriptionID, resourceGroupName, vmResourceType)
+
+	result := make([]compute.VirtualMachine, 0)
+	page := &VirtualMachineListResultPage{}
+	page.fn = c.listNextResults
+
+	resp, rerr := c.armClient.GetResourceWithExpandQuery(ctx, resourceID, "virtualMachineScaleSet/id eq "+vmssFlexID)
+	defer c.armClient.CloseResponse(ctx, resp)
+	if rerr != nil {
+		klog.V(5).Infof("Received error in %s: resourceID: %s, error: %s", "vm.list.request", resourceID, rerr.Error())
+		return result, rerr
+	}
+
+	var err error
+	page.vmlr, err = c.listResponder(resp)
+	if err != nil {
+		klog.V(5).Infof("Received error in %s: resourceID: %s, error: %s", "vm.list.respond", resourceID, err)
+		return result, retry.GetError(resp, err)
+	}
+
+	for {
+		result = append(result, page.Values()...)
+
+		// Abort the loop when there's no nextLink in the response.
+		if to.String(page.Response().NextLink) == "" {
+			break
+		}
+
+		if err = page.NextWithContext(ctx); err != nil {
+			klog.V(5).Infof("Received error in %s: resourceID: %s, error: %s", "vm.list.next", resourceID, err)
+			return result, retry.GetError(page.Response().Response.Response, err)
+		}
+	}
+
+	return result, nil
+}
+
 // Update updates a VirtualMachine.
 func (c *Client) Update(ctx context.Context, resourceGroupName string, VMName string, parameters compute.VirtualMachineUpdate, source string) *retry.Error {
 	mc := metrics.NewMetricContext("vm", "update", resourceGroupName, c.subscriptionID, source)
